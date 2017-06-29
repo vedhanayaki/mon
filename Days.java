f1a5b51a7a55d6f6bc1f69c9797e5873bb1c22b4
vedhanayaki <vedhanayaki1996@gmail.com>
2017-06-29 11:02:22 +0530

    Add files via upload

---
+++ Days.java
@@ -0,0 +1,40 @@
+package guviset3;
+import java.util.Scanner;
+public class Days {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		String s1=s.next();
+		switch(s1){
+		case "Mon":
+			System.out.println("true");
+			break;
+		case "Tue":
+			System.out.println("True");
+			break;
+		case "Wed":
+			System.out.println("True");
+			break;
+		case "Thu":
+			System.out.println("True");
+			break;
+		case "Fir":
+			System.out.println("True");
+			break;
+		case "Sat":
+			System.out.println("True");
+			break;
+		case "Sun":
+			System.out.println("False");
+			break;
+			default:
+				System.out.println("Null");
+		}
+		
+
+	
+
+	}
+
+}
